aa1667bf21106242febb6d745337faaa1fbd5d59
ZaidAli1994 <zaid.ali@xeleration.net>
2026-04-14 12:33:35 +0300

    fix by zaid

---
+++ tests/Loginpage.spec.ts
@@ -0,0 +1,11 @@
+import { test, expect } from '@playwright/test';
+
+
+
+test('login the page', async ({ page }) => {
+  await page.goto('https://qa.psi-crm.com/account/login');
+  await page.getByRole('textbox', { name: 'Username' }).fill('zaid');
+  await page.getByRole('textbox', { name: 'Password' }).fill('P@ssw0rdP@ssw0rd');
+  await page.getByRole('button', { name: 'Sign In' }).click();
+  await page.getByRole('button', { name: 'Verify & Login' }).click();
+});
